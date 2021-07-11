c70716bf0c7bf16fa7d97900e4a2c546e178baf4
siobhanlgorman <siobhanlgorman@gmail.com>
2021-07-11 10:58:45 +0000

    Add game part 3

--- run.py
+++ run.py
@@ -1,8 +1,15 @@
 def start_adventure():
-    print("You wake up in a dark, damp room.")
-    print("You don't know how you got there.")
+    #print("You wake up in a dark, damp room.")
+    #print("You don't know how you got there.")
     print("As your eyes adjust you can make out two doors, one on the left and one on the right.")
-    print(input("Which door do you choose?"))
+    door_picked = (input("Which door do you choose? Left or right?").lower())
+    if door_picked == "left":
+        print("You picked the door on the left")
+    elif door_picked == "right":
+        print("You picked the door on the right")
+    else:
+        print (input("Please pick left or right"))
+
 
 
 # def main():
