4e1a4e911be47bb8a836c44b2894d2c35f302480
siobhanlgorman <siobhanlgorman@gmail.com>
2021-07-11 12:53:45 +0000

    Add game part 6

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
+def you_die(why):
+    print(f"{why}")
+    exit()
+
+
 def start_adventure():
     
     door_picked = (input("Which door do you choose? Left or right?").lower())
@@ -6,6 +11,7 @@ def start_adventure():
         left_door()
     elif door_picked == "right":
         print("You picked the door on the right")
+        right_door()
     else:
         print (input("Please pick left or right"))
 
@@ -14,12 +20,24 @@ def left_door():
     print("Do you flee or fight?")
     next_move = input("> ").lower()
     if "flee" in next_move:
-        print("You chose to flee.\n You are back in the damp, dark room")
-        start_adventure()
+        print("You chose to flee.\n You are back in the damp, dark room.\nYou forgot to close the door.\n The two green eyes follow you and become a large scaly ...")
+        you_die("A ferocious large green dragon barbecues you")
     elif "fight" in next_move:
         print("You chose to fight")
     else:
         print("If you don't choose you die!")
+        you_die("A ferocious large green dragon barbecues you")
+        
+def right_door():
+    wooden_box = ["axe", "knife", "crossbow and arrows", "shotgun" ]
+    print("The door bangs open.\nInside the room there is a wooden box with the lid closed.\n You can hear footsteps approaching.\n")
+    # ask player what to do
+    action = input("What do you do? >")
+    # list of keywords to see user input
+    if action.lower() in ['box', 'open', 'lid']:
+        print("Quick! Let's see if there's a weapon in the box")
+    else:
+        print("Let's see who's coming ")
 
 
 
