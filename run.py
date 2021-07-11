933ebc71608284eb66261dc47c368961d55ca486
siobhanlgorman <siobhanlgorman@gmail.com>
2021-07-11 10:40:56 +0000

    Game parts 1-3

--- run.py
+++ run.py
@@ -1,1 +1,18 @@
-# python code goes here
+def start_adventure():
+    print("You wake up in a dark, damp room.")
+    print("You don't know how you got there.")
+    print("As your eyes adjust you can make out two doors, one on the left and one on the right.")
+    print(input("Which door do you choose?"))
+
+
+# def main():
+    
+#     #print(input("What's your name? >"))
+#     player_name = input("What's your name? >")
+#     print(f"Hi {player_name}!")
+#     player_character = input("What character would you like to be? E.g. knight, assassin >")
+#     print(f"Your name is {player_name.upper()}. You are a {player_character}! Cool!")
+start_adventure()
+
+# if __name__ == "__main__":
+#     main()
